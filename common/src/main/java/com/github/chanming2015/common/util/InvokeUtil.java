35a01923795ee18f103a25b110c550f4ff45bf73
chanming2015 <476076812@qq.com>
2016-03-04 20:46:30 +0800

    bug修复

--- common/src/main/java/com/github/chanming2015/common/util/InvokeUtil.java
+++ common/src/main/java/com/github/chanming2015/common/util/InvokeUtil.java
@@ -21,7 +21,6 @@ public class InvokeUtil
     private static final Logger log = LoggerFactory.getLogger(InvokeUtil.class);
 
     /**
-     * 
      * @author XuMaoSen
      * Create Time:2016年3月1日
      * Description 获取get方法
@@ -31,14 +30,12 @@ public class InvokeUtil
      */
     private static Method getGetMethod(Class<?> cls, String fieldName)
     {
-        Method method = null;
-        StringBuffer sb = new StringBuffer();
-        sb.append("get");
+        StringBuffer sb = new StringBuffer("get");
         sb.append(fieldName.substring(0, 1).toUpperCase(Locale.US));
         sb.append(fieldName.substring(1));
         try
         {
-            method = cls.getMethod(sb.toString());
+            return cls.getMethod(sb.toString());
         }
         catch (NoSuchMethodException e)
         {
@@ -48,11 +45,45 @@ public class InvokeUtil
         {
             log.error("getGetMethod SecurityException", e);
         }
-        return method;
+        return null;
     }
 
     /**
-     * 
+     * @author XuMaoSen
+     * Create Time:2016年3月2日
+     * Description 获取set方法
+     * @param cls
+     * @param fieldName
+     * @return
+     */
+    private static Method getSetMethod(Class<?> cls, String fieldName)
+    {
+        Class<?>[] parameterTypes = new Class<?>[1];
+        try
+        {
+            Field field = cls.getDeclaredField(fieldName);
+            parameterTypes[0] = field.getType();
+            StringBuffer sb = new StringBuffer("set");
+            sb.append(fieldName.substring(0, 1).toUpperCase(Locale.US));
+            sb.append(fieldName.substring(1));
+            return cls.getMethod(sb.toString(), parameterTypes);
+        }
+        catch (NoSuchFieldException e)
+        {
+            log.error("getSetMethod NoSuchFieldException", e);
+        }
+        catch (SecurityException e)
+        {
+            log.error("getSetMethod SecurityException", e);
+        }
+        catch (NoSuchMethodException e)
+        {
+            log.error("getSetMethod NoSuchMethodException", e);
+        }
+        return null;
+    }
+
+    /**
      * @author XuMaoSen
      * Create Time:2016年3月1日
      * Description 执行get方法
@@ -65,7 +96,10 @@ public class InvokeUtil
         Method method = getGetMethod(obj.getClass(), fieldName);
         try
         {
-            return method.invoke(obj, new Object[0]);
+            if (method != null)
+            {
+                return method.invoke(obj, new Object[0]);
+            }
         }
         catch (IllegalAccessException e)
         {
@@ -83,59 +117,22 @@ public class InvokeUtil
     }
 
     /**
-     * 
-     * @author XuMaoSen
-     * Create Time:2016年3月2日
-     * Description 获取set方法
-     * @param cls
-     * @param fieldName
-     * @return
-     */
-    private static Method getSetMethod(Class<?> cls, String fieldName)
-    {
-        Method method = null;
-        Class<?>[] parameterTypes = new Class<?>[1];
-        StringBuffer sb = new StringBuffer();
-        try
-        {
-            Field field = cls.getDeclaredField(fieldName);
-            parameterTypes[0] = field.getType();
-            sb.append("set");
-            sb.append(fieldName.substring(0, 1).toUpperCase(Locale.US));
-            sb.append(fieldName.substring(1));
-            method = cls.getMethod(fieldName, parameterTypes);
-        }
-        catch (NoSuchFieldException e)
-        {
-            log.error("getSetMethod NoSuchFieldException", e);
-        }
-        catch (SecurityException e)
-        {
-            log.error("getSetMethod SecurityException", e);
-        }
-        catch (NoSuchMethodException e)
-        {
-            log.error("getSetMethod NoSuchMethodException", e);
-        }
-        return method;
-    }
-
-    /**
-     * 
      * @author XuMaoSen
      * Create Time:2016年3月2日
      * Description 执行set方法
      * @param obj
      * @param fieldName
      * @param value
-     * @return
      */
-    public static Object invokeSet(Object obj, String fieldName, Object value)
+    public static void invokeSet(Object obj, String fieldName, Object value)
     {
         Method method = getSetMethod(obj.getClass(), fieldName);
         try
         {
-            return method.invoke(obj, new Object[] {value});
+            if (method != null)
+            {
+                method.invoke(obj, value);
+            }
         }
         catch (IllegalAccessException e)
         {
@@ -149,62 +146,73 @@ public class InvokeUtil
         {
             log.error("invokeSet InvocationTargetException", e);
         }
-        return null;
     }
 
-    public static List<String> getSetFieldList(Class<?> cls)
+    public enum MethodEnum
+    {
+        GET("get"), SET("set");
+
+        private String value;
+
+        private MethodEnum(String value)
+        {
+            this.value = value;
+        }
+
+        public String getValue()
+        {
+            return value;
+        }
+
+    }
+
+    /**
+     * @author XuMaoSen
+     * Create Time:2016年3月2日
+     * Description 获取该类的get或者set方法的字段的集合
+     * @param cls
+     * @param me
+     * @return
+     */
+    public static List<String> getFieldList(Class<?> cls, MethodEnum me)
     {
         List<String> fieldList = new ArrayList<String>();
         Method[] methods = cls.getMethods();
         for (Method method : methods)
         {
             String name = method.getName();
-            if (name.startsWith("set"))
+            if (name.startsWith(me.getValue()))
             {
                 fieldList.add(name.substring(3, 4).toLowerCase(Locale.US)
                         + name.substring(4));
             }
         }
-        return fieldList;
-    }
-
-    public static List<String> getGetFieldList(Class<?> cls)
-    {
-        List<String> fieldList = new ArrayList<String>();
-        Method[] methods = cls.getMethods();
-        for (Method method : methods)
+        if (MethodEnum.GET.equals(me))
         {
-            String name = method.getName();
-            if (name.startsWith("get"))
-            {
-                fieldList.add(name.substring(3, 4).toLowerCase(Locale.US)
-                        + name.substring(4));
-            }
+            fieldList.remove("class");
         }
         return fieldList;
     }
 
     /**
-     * 
      * @author XuMaoSen
      * Create Time:2016年3月2日
-     * Description 获取该类的有get和set方法的字段的集合
+     * Description 获取该类的get和set方法的字段的集合(同时包含get和set方法)
      * @param cls
      * @return
      */
     public static List<String> getFieldList(Class<?> cls)
     {
-        List<String> fieldSetList = getSetFieldList(cls);
-        List<String> fieldGetList = getGetFieldList(cls);
+        List<String> fieldSetList = getFieldList(cls, MethodEnum.SET);
+        List<String> fieldGetList = getFieldList(cls, MethodEnum.GET);
 
-        List<String> fieldList = new ArrayList<String>();
-        for (String str : fieldGetList)
-        {
-            if (fieldSetList.contains(str))
-            {
-                fieldList.add(str);
-            }
-        }
-        return fieldList;
+        fieldSetList.retainAll(fieldGetList);
+
+        return fieldSetList;
+    }
+
+    public static void main(String[] args)
+    {
+        getFieldList(Integer.class);
     }
 }
